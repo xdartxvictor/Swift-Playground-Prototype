59b4040edbb6f63de2addeca84ba0a656f868d37
victor <xdartxvictor@gmail.com>
2017-12-21 08:45:19 -0500

    Step 2

--- MyPrototype.playground/Contents.swift
+++ MyPrototype.playground/Contents.swift
@@ -2,4 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class ViewController : UIViewController{
+    var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad() // llamamos a la superclase
+        self.view.frame = CGRect(x: 0, y: 0, width: 320, height: 480) //definimos las dimensiones de la vista
+        self.tableView = UITableView(frame:self.view.frame) //  inicializamos nuestra tabla con las dimenciones de la vista
+        // definimos algunos
+        self.tableView.separatorStyle = .none  // eliminamos el separador de las celdas
+        self.tableView.rowHeight = 50.0  // definimos el alto de la celda
+        self.tableView.backgroundColor = UIColor.black  // definimos el color de fondo de la tabla en color negro
+        self.view.addSubview(self.tableView)  // agregamos la tabla a la vista
+    }
+}
